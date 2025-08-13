015bdfdb30c56d652efff5326a2a4ffa2071927b
aishanaaz19 <ayeshanaaz396@gmail.com>
2025-08-13 13:00:51 +0530

    ,,

--- index.js
+++ index.js
@@ -37,4 +37,4 @@ app.post("/send-message", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
